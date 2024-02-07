25c468a46669cd142dfc5e065afc87e0057e8253
danielnaiff <daniel.naiff@yahoo.com.br>
2024-02-07 18:11:57 -0300

    printf();

---
+++ printf/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\nValor retornando: %d", printf("Bom"));
+
+    printf("\nPressione qualquer tecla para finalizar.");
+    getchar();
+}
