4bdc370c9871a93c979cfad7fe2a5c53ea53b118
DanielNaiff <danielnaiff344@gmail.com>
2024-04-20 22:11:41 -0300

    aula 16

---
+++ class 16 - Como descobrir o tamanho em memória de cada tipo de dado/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  float x = 1.0;
+
+  printf("Tamanho em memoria de um float: %d bytes\n", sizeof(x));
+  printf("Tamanho em memoria de um int: %d bytes\n", sizeof(int));
+
+  return 0;
+}
